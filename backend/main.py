6de45930c3de19fd579c7eb17dfe469b09ab7841
fullstackdev07 <fullstackdev07@gmail.com>
2025-07-26 19:19:59 +0530

    updated the backend server

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5173","http://0.0.0.0:5173"],
+    allow_origins=["http://127.0.0.1:5173","http://0.0.0.0:5173","http://127.0.0.1:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
